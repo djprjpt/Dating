793fef18f47dcd1e6d3bbac7a242a33466e8c0d8
Dhaval Prajapati <dhaval.prjpt@gmail.com>
2017-05-18 13:40:01 +0530

    folder structure
    folder structure commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Dhaval Prajapati
+ * Dating: Index.php with the great commitment and great dedication, I, Dhaval Prajapati starting this project in hope of a completion.
+ * Responsibility: this page will check the url response and redirect it to respective module.
+ * 
+ */
+
